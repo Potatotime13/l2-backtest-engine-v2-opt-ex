1fa43206501f2c6fceb8f36e2e42cbff60dba7ff
Markus Diebels <markus.diebels@freenet.de>
2022-06-07 22:07:32 +0200

    Pep 8 schedule.py

--- agent/schedule.py
+++ agent/schedule.py
@@ -1,6 +1,6 @@
 # general imports
 from __future__ import annotations
-import random as rn
+# import random as rn
 import numpy as np
 import pandas as pd
 from typing import TYPE_CHECKING
@@ -10,25 +10,30 @@ from env.market import MarketState
 
 # quality of life imports
 if TYPE_CHECKING:
-    from agent.parent_order import Parent_order
+    from agent.parent_order import ParentOrder
 
-class Schedule():
-    '''
-    A Schedule object is a nessesary part of each parant order and manages the child order
+
+class Schedule:
+    """
+    A Schedule object is a necessary part of each parent order and manages
+    the child order
     timing of it
-    '''
-    def __init__(self, parent_order:Parent_order, child_window:int, pattern:list) -> None:
-        '''
-        mehtod that defines a time schedule per parent order which is called by the order itself
+    """
+    def __init__(self, parent_order: ParentOrder, child_window: int,
+                 pattern: list) -> None:
+        """
+        method that defines a time schedule per parent order which is called
+        by the order itself
 
         :param parent_order:
             Parent_order, object of the parent order which asks for a schedule
         :param child_window:
             int, length of a one point in the schedule to the next in minutes
         :param pattern:
-            list[number], list of numbers which scetch a pattern for the order volumes
+            list[number], list of numbers which sketch a pattern for the order
+            volumes
             example: [1] -> linear pattern, [1,2] -> increasing pattern
-        '''
+        """
         # generate timestamps
         # account for across day time windows
         time_stamps = []
@@ -41,11 +46,19 @@ class Schedule():
         self.pattern = pattern
 
         while time_unreached:
-            if self.start_time + pd.DateOffset(minutes=self.child_window * count) < parent_order.MARKET_END:
-                offset_time = self.start_time + pd.DateOffset(minutes=self.child_window * count)
+            if self.start_time \
+                    + pd.DateOffset(minutes=self.child_window * count) \
+                    < parent_order.MARKET_END:
+                offset_time = self.start_time \
+                              + pd.DateOffset(
+                                minutes=self.child_window * count)
             else:
-                offset_time = parent_order.NEXT_MARKET_START + ((self.start_time + pd.DateOffset(minutes=self.child_window * count)) - parent_order.MARKET_END)
-            if   offset_time > self.end_time:
+                offset_time = parent_order.NEXT_MARKET_START \
+                              + ((self.start_time
+                                  + pd.DateOffset(
+                                    minutes=self.child_window * count))
+                                 - parent_order.MARKET_END)
+            if offset_time > self.end_time:
                 time_unreached = False
             else:
                 time_stamps.append(offset_time)
@@ -53,47 +66,55 @@ class Schedule():
         
         # meta schedule
         diff = int((self.end_time-self.start_time).seconds / 3600)
-        self.meta_schedule_time = [self.start_time+pd.DateOffset(hours=i) for i in range(1,diff)]
+        self.meta_schedule_time = [self.start_time+pd.DateOffset(hours=i)
+                                   for i in range(1, diff)]
         self.meta_schedule_time.append(self.end_time-pd.DateOffset(minutes=5))
         self.meta_schedule = pd.DataFrame(
-            {'time':self.meta_schedule_time, 
-            'Done':[False for _ in range(len(self.meta_schedule_time))]}
+            {'time': self.meta_schedule_time,
+             'Done': [False for _ in range(len(self.meta_schedule_time))]}
         )
         self.meta_schedule = self.meta_schedule.set_index('time')
 
         # pattern to distribution
-        x = np.linspace(0,1,len(pattern))
+        x = np.linspace(0, 1, len(pattern))
         y = np.array(pattern)
-        x_inter = np.linspace(0,1,len(time_stamps))
-        y_inter = np.interp(x_inter,x,y)
+        x_inter = np.linspace(0, 1, len(time_stamps))
+        y_inter = np.interp(x_inter, x, y)
         percentages = y_inter/np.sum(y_inter)
 
         # calculate volumes
-        volumes = np.maximum(parent_order.volume*percentages,1).astype(int)
+        volumes = np.maximum(parent_order.volume*percentages, 1).astype(int)
         vol_left = int(parent_order.volume - np.sum(volumes))
         if vol_left > 0:
             volumes[np.arange(vol_left)] += 1
         elif vol_left < 0:
-            volumes[np.arange(len(time_stamps)+vol_left,len(time_stamps))] -= 1
-        self.scheduling = pd.DataFrame({'timestamp':time_stamps,'volume':volumes.tolist()})
+            volumes[np.arange(len(time_stamps) +
+                              vol_left, len(time_stamps))] -= 1
+        self.scheduling = pd.DataFrame({'timestamp': time_stamps,
+                                        'volume': volumes.tolist()})
 
-    def stay_scheduled(self, market_state:MarketState, timestamp:pd.Timestamp) -> dict:
-        '''
+    def stay_scheduled(self, market_state: MarketState,
+                       timestamp: pd.Timestamp) -> dict:
+        """
         method to send market orders to stay on the planed schedule
 
         :params timestamp:
             pd.Timestamp, moment of method call usually called out of on_time
-        '''
+        """
 
-        if np.all(self.meta_schedule[self.meta_schedule.index<timestamp]):
-            limit = market_state.best_ask if self.parent_order.side == 'buy' else market_state.best_bid
+        if np.all(self.meta_schedule[self.meta_schedule.index < timestamp]):
+            if self.parent_order.side == 'buy':
+                limit = market_state.best_ask
+            else:
+                limit = market_state.best_bid
         else:
             limit = None
-            self.meta_schedule[self.meta_schedule.index<timestamp]=True
+            self.meta_schedule[self.meta_schedule.index < timestamp] = True
 
         output = None
         if self.parent_order.active:
-            orders_to_fill = self.scheduling[self.scheduling['timestamp']<timestamp]            
+            orders_to_fill = self.scheduling[self.scheduling['timestamp']
+                                             < timestamp]
             if orders_to_fill['volume'].sum() > 0:
                 output = {
                     'symbol': self.parent_order.symbol,
@@ -104,24 +125,26 @@ class Schedule():
                 }
         return output
 
-    def reduce_schedule(self, reduce_volume:float) -> None:
-        '''
+    def reduce_schedule(self, reduce_volume: float) -> None:
+        """
         method to actualize the schedule after an order was executed
 
         :param reduce_volume:
             float, number of shares traded
-        '''
+        """
         for index, vol in self.scheduling['volume'].iteritems():
             if 0 < vol <= reduce_volume:
                 reduce_volume -= vol
-                self.scheduling.loc[index,'volume'] = 0
+                self.scheduling.loc[index, 'volume'] = 0
             elif vol > reduce_volume:
-                self.scheduling.loc[index,'volume'] -= reduce_volume
+                self.scheduling.loc[index, 'volume'] -= reduce_volume
                 reduce_volume = 0
     
     def get_outstanding(self, timestamp) -> float:
-        if self.scheduling[self.scheduling['timestamp']<timestamp].index.empty:
+        if self.scheduling[self.scheduling['timestamp']
+                           < timestamp].index.empty:
             ind = 0
         else:
-            ind = max(self.scheduling[self.scheduling['timestamp']<timestamp].index)+1
-        return self.scheduling.loc[:ind,'volume'].sum()
+            ind = max(self.scheduling[self.scheduling['timestamp']
+                                      < timestamp].index)+1
+        return self.scheduling.loc[:ind, 'volume'].sum()
