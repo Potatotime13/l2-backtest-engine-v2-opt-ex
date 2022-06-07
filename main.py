08c601f000fb5fb6c81e50aa8c3f9b26e5ac7e14
Markus Diebels <markus.diebels@freenet.de>
2022-06-07 22:37:17 +0200

    Pep 8 main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import re
 # project imports
 from agent.agent import BaseAgent
 from env.replay import Backtest
-from agent.decission_support import Decission_support
-from agent.order_management import Order_Management_System
+from agent.decission_support import DecisionSupport
+from agent.order_management import OrderManagementSystem
 
 
 class Agent(BaseAgent):
@@ -18,9 +18,10 @@ class Agent(BaseAgent):
         """
         Trading agent implementation.
 
-        The backtest iterates over a set of sources and alerts the trading agent
-        whenever a source is updated. These updates are conveyed through method
-        calls to, all of which you are expected to implement yourself: 
+        The backtest iterates over a set of sources and alerts the trading
+        agent whenever a source is updated. These updates are conveyed
+        through method calls to, all of which you are expected to
+        implement yourself:
 
         - on_quote(self, market_id, book_state)
         - on_trade(self, market_id, trade_state)
@@ -62,53 +63,55 @@ class Agent(BaseAgent):
         """
         super(Agent, self).__init__(name, *args, **kwargs)
 
-        ### extract stock list out of identifier list
+        # extract stock list out of identifier list
         self.stock_list = []
         for val in identifies:
             stock = re.split(r'\.(?!\d)', val)[0]
             if len(self.stock_list) == 0 or self.stock_list[-1] != stock: 
                 self.stock_list.append(stock)
 
-        # Decsission support
-        self.support = Decission_support(self.stock_list, None)
+        # Decision support
+        self.support = DecisionSupport(self.stock_list, None)
 
-        ### variable parameter set
-        self.order_management = Order_Management_System(self.stock_list, self)
+        # variable parameter set
+        self.order_management = OrderManagementSystem(self.stock_list, self)
         self.check_status = None        
 
-        ### static parameter set
-        self.vol_range = [0.05,0.07]    # percent of daily vol
+        # static parameter set
+        self.vol_range = [0.05, 0.07]    # percent of daily vol
         self.time_window = 1            # hours
         self.child_window = 2           # minutes TODO: maybe dynamic per stock
 
-    def on_quote(self, market_id:str, book_state:pd.Series):
+    def on_quote(self, market_id: str, book_state: pd.Series):
         """
         This method is called after a new quote.
 
         :param market_id:
             str, market identifier
         :param book_state:
-            pd.Series, including timestamp, bid/ask price/quantity for 10 levels
+            pd.Series, including timestamp,
+            bid/ask price/quantity for 10 levels
         """
         if self.order_management.initialized:
             if self.order_management.get_recent_parent_order(market_id).active:
-                self.update_limit_order(market_id, book_state['TIMESTAMP_UTC'], book_state)
+                self.update_limit_order(market_id, book_state['TIMESTAMP_UTC'],
+                                        book_state)
 
-    def on_trade(self, market_id:str, trades_state:pd.Series):
+    def on_trade(self, market_id: str, trades_state: pd.Series):
         """
         This method is called after a new trade.
 
         :param market_id:
             str, market identifier
         :param trades_state:
-            pd.Series: TIMESTAMP_UTC :pd.Timestamp, Price :[float], Volume :[float]
+            pd.Series: TIMESTAMP_UTC :pd.Timestamp,
+            Price :[float], Volume :[float]
         """
 
         # calculate vwap for active order
         self.order_management.update_vwap(market_id, trades_state)
 
-
-    def on_time(self, timestamp:pd.Timestamp, timestamp_next:pd.Timestamp):
+    def on_time(self, timestamp: pd.Timestamp, timestamp_next: pd.Timestamp):
         """
         This method is called with every iteration and provides the timestamps
         for both current and next iteration. The given interval may be used to
@@ -125,7 +128,8 @@ class Agent(BaseAgent):
 
         # check every minute if orders are outdated
         # check if they are in their schedule         
-        if self.check_status == None or self.check_status + pd.DateOffset(minutes=1) < timestamp:
+        if self.check_status is None \
+                or self.check_status + pd.DateOffset(minutes=1) < timestamp:
             self.check_status = timestamp
             print(timestamp)
             self.order_management.check_status_on_time(timestamp)
@@ -143,41 +147,48 @@ class Agent(BaseAgent):
                 )
 
     def update_limit_order(self, market_id, timestamp, market_state):
-        '''
-        method to send limit orders to the market, dependend on the agent strategy
-        '''
-        orders = self.market_interface.get_filtered_orders(market_id, status="ACTIVE")
+        """
+        method to send limit orders to the market,
+        dependent on the agent strategy
+        """
+        orders = self.market_interface.get_filtered_orders(market_id,
+                                                           status="ACTIVE")
         limit_order = False
         create_new_order = False
         for order in orders:
-            if not order.limit is None:
+            if order.limit is not None:
                 limit_order = True
                 if self.support.update_needed(market_state, order, timestamp):
                     print(
                         'order position: ', 
                         self.order_management.get_order_book_position(
-                            self.market_interface.market_state_list[order.market_id], order
+                            self.market_interface.market_state_list[
+                                order.market_id], order)
                     )
-                )
                     self.market_interface.cancel_order(order)
                     create_new_order = True
 
         if not limit_order or create_new_order:
-            parent_order = self.order_management.get_recent_parent_order(market_id)
-            if parent_order.schedule.get_outstanding(timestamp)>0:
-                parent_order.child_orders.append(self.market_interface.submit_order(
-                    market_id=market_id, 
-                    side=parent_order.side, 
-                    quantity=parent_order.schedule.get_outstanding(timestamp),
-                    limit=self.support.determinate_price(market_state, parent_order),
-                    parent=parent_order
-                ))
-
+            parent_order = \
+                self.order_management.get_recent_parent_order(market_id)
+            if parent_order.schedule.get_outstanding(timestamp) > 0:
+                parent_order.child_orders.append(
+                    self.market_interface.submit_order(
+                        market_id=market_id,
+                        side=parent_order.side,
+                        quantity=parent_order.schedule.get_outstanding(
+                            timestamp),
+                        limit=self.support.determinate_price(market_state,
+                                                             parent_order),
+                        parent=parent_order
+                    )
+                )
 
     def update_schedule(self, parent_order):
-        '''
-        method that allows more flexible scheduling, like front and back loading
-        '''
+        """
+        method that allows more flexible scheduling,
+        like front and back loading
+        """
         pass
 
 
@@ -187,25 +198,25 @@ if __name__ == "__main__":
 
     identifier_list = [
         # ADIDAS
-        #"Adidas.BOOK", "Adidas.TRADES",
+        # "Adidas.BOOK", "Adidas.TRADES",
         # ALLIANZ
-        #"Allianz.BOOK", "Allianz.TRADES",
+        # "Allianz.BOOK", "Allianz.TRADES",
         # BASF
-        #"BASF.BOOK", "BASF.TRADES",
+        # "BASF.BOOK", "BASF.TRADES",
         # Bayer
-        #"Bayer.BOOK", "Bayer.TRADES",
+        # "Bayer.BOOK", "Bayer.TRADES",
         # BMW
-        #"BMW.BOOK", "BMW.TRADES",
+        # "BMW.BOOK", "BMW.TRADES",
         # Continental
-        #"Continental.BOOK", "Continental.TRADES",
+        # "Continental.BOOK", "Continental.TRADES",
         # Covestro
-        #"Covestro.BOOK", "Covestro.TRADES",
+        # "Covestro.BOOK", "Covestro.TRADES",
         # Daimler
         "Daimler.BOOK", "Daimler.TRADES",
         # Deutsche Bank
-        #"DeutscheBank.BOOK", "DeutscheBank.TRADES",
+        # "DeutscheBank.BOOK", "DeutscheBank.TRADES",
         # DeutscheBörse
-        #"DeutscheBörse.BOOK", "DeutscheBörse.TRADES",
+        # "DeutscheBörse.BOOK", "DeutscheBörse.TRADES",
     ]
 
     # TODO: INSTANTIATE AGENT. Please refer to the corresponding file for more 
@@ -249,11 +260,8 @@ if __name__ == "__main__":
     # Option 3: run agent against a series of specified episodes, that is, 
     # list a tuple (episode_start_buffer, episode_start, episode_end) for each 
     # episode
-    backtest.run_episode_list(identifier_list=identifier_list,
-        episode_list=[
-            ("2021-01-04T08:00:00", "2021-01-04T08:15:00", "2021-01-04T09:30:00"),
-            # ... 
+    backtest.run_episode_list(identifier_list=identifier_list, episode_list=[
+        ("2021-01-04T08:00:00", "2021-01-04T08:15:00", "2021-01-04T09:30:00"),
+        #  ...
         ],
     )
-
-
