a196cf9ecbe1a8a4f0cbab74991f3ce84b18880b
Markus Diebels <markus.diebels@freenet.de>
2022-06-07 21:44:30 +0200

    Pep 8 decision_support.py

--- agent/decission_support.py
+++ agent/decission_support.py
@@ -1,8 +1,8 @@
 # general imports
 from __future__ import annotations
 import pandas as pd
-import numpy as np
-import random as rn
+# import numpy as np
+# import random as rn
 from typing import Union, TYPE_CHECKING
 
 # project imports
@@ -14,45 +14,58 @@ if TYPE_CHECKING:
     from agent.parent_order import Parent_order
 
 
-class Decission_support():
+# TODO Prüfen, ob das einen Fehler gibt.
+# Alter name war Decission_support()
+class DecisionSupport:
 
-    def __init__(self, stock_list, agressivness) -> None:
+    def __init__(self, stock_list, aggressiveness) -> None:
         self.ml_model_a = None
         self.ml_model_b = None
         self.stock_list = stock_list
-        self.agressivness = agressivness
-        self.vol_dist = pd.read_csv('./agent/resources/daily_volume.csv').set_index('Unnamed: 0')
+        self.aggressiveness = aggressiveness
+        self.vol_dist = pd.read_csv('./agent/resources/'
+                                    'daily_volume.csv').set_index('Unnamed: 0')
 
-    def get_volume_distribution(self, stock:str, timestamp:pd.Timestamp, window:int) -> list:
+    def get_volume_distribution(self, stock: str, timestamp: pd.Timestamp,
+                                window: int) -> list:
         hour = timestamp.hour-8
-        return self.vol_dist.loc[hour:hour+window,stock].astype(int)
+        return self.vol_dist.loc[hour:hour+window, stock].astype(int)
     
     def get_daily_volume(self, stock):
         return self.vol_dist.mean()[stock]
 
-    def update_needed(self, market_state, order:Order, timestamp):
+    @staticmethod
+    def update_needed(market_state, order: Order, timestamp) -> bool:
         trigger = False
         level = '3'
         side = 'Bid' if order.side == 'buy' else 'Ask'
-        if  market_state['L'+level+'-'+side+'Price'] != order.limit and order.parent.schedule.get_outstanding(timestamp)>0:
+        if market_state['L'+level+'-'+side+'Price'] != \
+                order.limit and \
+                order.parent.schedule.get_outstanding(timestamp) > 0:
             trigger = True
         return trigger
 
-    def determinate_price(self, market_state, order: Union[Order, Parent_order]):
+    @staticmethod
+    def determinate_price(market_state,
+                          order: Union[Order, Parent_order]):
         level = '3'
         side = 'Bid' if order.side == 'buy' else 'Ask'
         return market_state['L'+level+'-'+side+'Price']
     
-    def get_alpha(self, side, stock, book_state):
+    @staticmethod
+    def get_alpha(side, stock, book_state):
         timestamp = book_state['TIMESTAMP_UTC']
         direction = everknowing_entity(timestamp, stock, 500)
         # edge would be the standard deviation used for the label
         if side == 'sell':
             alpha = direction * -1
+        # TODO Ist das hier richtig? Alpha war vorher nur im if Teil zugewiesen
+        else:
+            alpha = direction
         return alpha
 
-    def get_intensity():
+    def get_intensity(self):
         pass
 
-    def get_execution_prob():
+    def get_execution_prob(self):
         pass
